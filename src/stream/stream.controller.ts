3e0bd4140ef7d0c7f1a34e02d21ca1d100af36ff
sakib-siddiqi <sakibsiddiqi15@gmail.com>
2025-10-20 22:34:39 +0600

    fix : redirect to url

--- src/stream/stream.controller.ts
+++ src/stream/stream.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Redirect } from '@nestjs/common';
 import { StreamService } from './stream.service';
 
 @Controller('stream')
@@ -6,26 +6,31 @@ export class StreamController {
     constructor(private readonly streamService: StreamService) { }
 
     @Get('/movie/:id')
+    @Redirect()
     async getMovieStreamUrl(
         @Param('id', ParseIntPipe) id: number
     ) {
-        return this.streamService.getStreamUrl({
+        const streamUrl = await this.streamService.getStreamUrl({
             type: 'movie',
             id,
         });
+        return { url: streamUrl, statusCode: 302 };
     }
 
     @Get('/tv-series/:id/:seasonNumber/:episodeNumber')
+    @Redirect()
     async getTVShowStreamUrl(
         @Param('id', ParseIntPipe) id: number,
         @Param('seasonNumber', ParseIntPipe) seasonNumber: number,
         @Param('episodeNumber', ParseIntPipe) episodeNumber: number
     ) {
-        return this.streamService.getStreamUrl({
+        const streamUrl = await this.streamService.getStreamUrl({
             type: 'tv-series',
             id,
             seasonNumber,
             episodeNumber
         });
+
+        return { url: streamUrl, statusCode: 302 };
     }
 }
